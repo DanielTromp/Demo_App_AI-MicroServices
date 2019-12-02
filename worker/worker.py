f904e35efab44f79bfef1370aba2529006f1d160
Daniel Tromp <drpgmtromp@gmail.com>
2019-12-02 17:18:31 +0100

    Add files via upload

--- worker/worker.py
+++ worker/worker.py
@@ -17,13 +17,13 @@ from mimetypes import MimeTypes
 from tempfile import NamedTemporaryFile
 from os import listdir
 
-minio = Minio('minio:9000',
+minio = Minio('localhost:9000',
                 access_key='access_key',
                 secret_key='secret_key',
                 secure=False)
 
-celery = Celery(broker='redis://redis:6379/0', 
-                backend='redis://redis:6379/0')
+celery = Celery(broker='redis://localhost:6379/0', 
+                backend='redis://localhost:6379/0')
 
 celery_logger = get_task_logger(__name__)
 
@@ -104,6 +104,7 @@ def ageModel():
     
     age_model = Model(inputs=model.input, outputs=base_model_output)
     age_model.load_weights("models/age_model_weights.h5")
+    age_model._make_predict_function()
     
     return age_model
 
@@ -117,6 +118,7 @@ def genderModel():
 
     gender_model = Model(inputs=model.input, outputs=base_model_output)
     gender_model.load_weights("models/gender_model_weights.h5")
+    gender_model._make_predict_function()
     
     return gender_model
 
@@ -124,6 +126,7 @@ def emotionModel():
     emotion_model = model_from_json(
         open("models/facial_expression_model_structure.json", "r").read())
     emotion_model.load_weights("models/facial_expression_model_weights.h5") 
+    emotion_model._make_predict_function()
 
     return emotion_model
 
@@ -185,6 +188,7 @@ def processing(filename):
 
             except:
                 print("detected face has no margin")
+            print("#")
             
             try:
                 #vgg-face expects inputs (224, 224, 3)
@@ -192,17 +196,20 @@ def processing(filename):
                 img_pixels = image.img_to_array(detected_face)
                 img_pixels = np.expand_dims(img_pixels, axis = 0)
                 img_pixels /= 255
+                print("##")
 
                 #find out age and gender
-                with graph.as_default():
-                    age_distributions = age_model.predict(img_pixels)
-                    gender_distribution = gender_model.predict(img_pixels)[0]
-                    emotion_distribution = emotion_model.predict(emotion_pixels)
+                #with graph.as_default():
+                age_distributions = age_model.predict(img_pixels)
+                gender_distribution = gender_model.predict(img_pixels)[0]
+                emotion_distribution = emotion_model.predict(emotion_pixels)
+                print("###")
 
                 apparent_age = str(int(np.floor(np.sum(age_distributions * output_indexes, axis = 1))[0]))
                 gender_index = np.argmax(gender_distribution)
                 if gender_index == 0: gender = "F"
                 else: gender = "M"
+                print("####")
 
                 max_index = np.argmax(emotion_distribution[0])
                 emotion = emotions[max_index]
@@ -215,6 +222,7 @@ def processing(filename):
                 cv2.drawContours(img, [triangle_cnt], 0, info_box_color, -1)
                 cv2.rectangle(img,(x+int(w/2)-50,y-10),(x+int(w/2)+55,y-65),
                     info_box_color,cv2.FILLED)
+                print("#####")
                 
                 if enableGenderIcons:
                     if gender == 'M': gender_icon = male_icon
@@ -223,15 +231,18 @@ def processing(filename):
                     img[y-60:y-60+male_icon.shape[0], x+int(w/2)-50:x+int(w/2)-50+male_icon.shape[1]] = gender_icon
                 else:
                     cv2.putText(img, gender, (x+int(w/2)-42, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                print("######")
 
                 #labels for age and gender
                 cv2.putText(img, apparent_age, (x+int(w/2), y - 35), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
                 cv2.putText(img, emotion, (x+(int(w/2)-15), y - 20), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                print("#######")
 
             except Exception as e:
                 print("exception",str(e))
+
 
     cv2.imwrite(tmp.name, img)
     # OpenCV script End #
